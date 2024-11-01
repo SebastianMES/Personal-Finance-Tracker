d32758de437887e4901bed508039228e1a7681f7
Grace Jeong <dbwjd1742@gmail.com>
2024-10-31 23:06:15 -0700

    Fixed errors and imporved functions

--- Globals.py
+++ Globals.py
@@ -1,5 +1,5 @@
 import pandas as pd
 from datetime import datetime
-transaction_df = pd.DataFrame(columns=['Date','Category', 'Description', 'Amount','Type'])
+transaction_df = pd.DataFrame(columns=['Date', 'Category', 'Description', 'Amount', 'Type'])
 today_date = datetime.today().date()
 changes_unsaved = False
